edbe8dd485f49c17e580b958c879253b2f419862
sonatrachhq <sonatrachhq@gmail.com>
2021-01-04 14:54:58 +0100

    version2.0

--- frontend/src/app/file-to-print-settings/file-to-print-settings.component.ts
+++ frontend/src/app/file-to-print-settings/file-to-print-settings.component.ts
@@ -1,3 +1,5 @@
+import { element } from 'protractor';
+import { map } from 'rxjs/operators';
 import { ShActivity } from './../Models/ShActivity';
 import { FileToPrint } from './../Models/FileToPrint';
 import { Component, OnInit, AfterViewInit, AfterContentChecked, AfterViewChecked } from '@angular/core';
@@ -20,188 +22,33 @@ export interface Element {
   templateUrl: './file-to-print-settings.component.html',
   styleUrls: ['./file-to-print-settings.component.css']
 })
-export class FileToPrintSettingsComponent implements OnInit, AfterViewChecked, AfterContentChecked {
-  @ViewChildren("checkboxes", { read: MatCheckbox }) checkboxes: QueryList<MatCheckbox> = new QueryList;
-  fileToPrintFormGroup: FormGroup = new FormGroup({});
-  strucures: Structure[] = [];
-  toutLesEtats: clotureFiles[] = [];
-  finalEtatSelected: FileToPrint[] = [];
-  finalEtatDeselected: FileToPrint[] = [];
+export class FileToPrintSettingsComponent implements OnInit{
+  payrollstructures: Structure[] = [];
+  tousLesEtats: clotureFiles[] = [];  
+  allShActivities: ShActivity[] = [];
   allFileToPrint: FileToPrint[] = [];
-  intermediaireFileToPrint: FileToPrint[] = [];
-  selectedStructureIds: [] = [];
-  selectedEtatsIds: [] = [];
-  exist: Boolean = false;
-  index: number = 0;
-  dontPush: Boolean = false;
-  initialisation: Boolean = true;
+  tempFileToPrint: FileToPrint[] = [];
+  messageAdded:Boolean=false;
+  messageDeleted:Boolean=false;
+ 
   formSettings: MbscFormOptions = {
     theme: 'mobiscroll',
     themeVariant: 'light'
   };
-
-  etatSelectedForStructure: Element[] = [];
-  selectedCheckBoxes: [number] = [0];
-  size: number = 0;
-  allShActivities: ShActivity[] = [];
-  get etatFormArray() {
-    return this.fileToPrintFormGroup.controls['etatSelected'] as FormArray;
-  }
-  get structureFormArray() {
-    return this.fileToPrintFormGroup.controls['structureSelected'] as FormArray;
-  }
+  fileToPrintFormGroup: FormGroup = new FormGroup({});
   constructor(private _formBuilder: FormBuilder, private comService: CommunicationService) {
 
 
   }
 
-  addToggles() {
-    console.log("toggles");
-    let i: number = 0;
-    let exist: Boolean = false;
-    //
-    for (let j = 0; j < this.strucures.length; j++) {
-      i = 0;
-
-      while (i < this.allFileToPrint.length) {
-        exist = false;
-        if (this.strucures[j].idstructure == this.allFileToPrint[i].idStructure) {
-
-          exist = true;
-          break;
-        }
-        i++;
-      }
-      if (!exist) {
-        this.structureFormArray.push(new FormControl(false))
-
-
-      } else {
-        this.structureFormArray.push(new FormControl(true))
-
-      }
-
-
-    }
-
-
-  }
-
-  getAllSelectedEtatWithStructure() {
-    console.log("structureswithEtat");
-    let i = 0;
-    for (i; i < this.strucures.length; i++) {
-      let file: FileToPrint = {
-        "idFileType": i,
-        "idStructure": this.strucures[i].idstructure,
-        "addedDate": new Date()
-      };
-
-      this.getEtatsPrinted(file);
-    }
-
-
-  }
-
-
-  getEtatsPrinted(file: FileToPrint) {
-    console.log("getEtatsPrinted")
-    let ids: [number] = [0];
-    let idS: number;
-    ids.pop();
-    this.comService.getSelectedEtat(file).subscribe(
-      data => {
-
-        if (data.length != 0) {
-
-          if (data.length == 1) {
-            this.etatSelectedForStructure.push({
-              "idStructure": data[0].idStructure,
-              "idEtat": [data[0].idFileType]
-            });
-          }
-          let i = 1;
-          while (i < data.length) {
-
-
-            if (data[i - 1].idStructure == data[i].idStructure) {
-              ids.push(data[i - 1].idFileType);
-              idS = file.idStructure;
-
-            }
-            i++;
-            // console.log(data[i].idStructure+"  "+data[i+1].idStructure);
-
-          }
-
-          ids.push(data[data.length - 1].idFileType);
-
-          this.size = this.etatSelectedForStructure.push({
-            "idStructure": idS,
-            "idEtat": ids
-          });
-          //  console.log(this.etatSelectedForStructure);
-
-        }
-
-        if (file.idStructure == this.strucures.length) {
-          this.addCheckboxes();
-
-        }
-
-
-      },
-      error => {
-        console.log(error);
-        alert(error);
-        throw error;
-
-      }
-    );
-
-
-  }
-  private addCheckboxes() {
-
-    for (let k = 0; k < this.toutLesEtats.length; k++) {
-
-      this.etatFormArray.push(new FormControl(false));
-    }
-
-
-  }
-
-
-
-  isChecked(idStructure: number, idEtat: number): Boolean {
-    for (let i = 0; i < this.etatSelectedForStructure.length; i++) {
-      if (this.etatSelectedForStructure[i].idStructure == idStructure) {
-        if (this.etatSelectedForStructure[i].idEtat.indexOf(idEtat) != -1) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  ngAfterViewChecked() {
-
-  }
-  ngAfterContentChecked() {
-
-
-  }
-
   ngOnInit(): void {
-    console.log("onInit");
-    //get all structures pour les file to print
+
     this.comService.getAllStructures().subscribe(
       (data) => {
-        this.strucures = data;
+        this.payrollstructures = data;
+        console.log("allstructures");
         console.log(data);
         //this.getAllSelectedEtatWithStructure();
-
-
       },
       error => {
         console.log(error);
@@ -210,51 +57,57 @@ export class FileToPrintSettingsComponent implements OnInit, AfterViewChecked, A
 
       }
     );
-
-
-    //initilaisation du form pour selection de la structure et des etats
-    this.fileToPrintFormGroup = this._formBuilder.group({
-      structureSelected: new FormArray([]),
-      etatSelected: new FormArray([])
-    });
-
-
-
-
 
     //get all etats to select file to print
 
     this.comService.getEtats().subscribe(
       (data) => {
-        this.toutLesEtats = data;
-        this.addToggles();
-        this.getAllSelectedEtatWithStructure();
+        this.tousLesEtats = data;
+        //this.addToggles();
+        //this.getAllSelectedEtatWithStructure();
+        console.log("touslesetats");
         console.log(data);
-
-
       },
       error => {
         console.log(error);
         alert(error);
         throw error;
-
       }
     );
 
     //get all files to print
     this.comService.getAllFileToPrint().subscribe(
       (data) => {
-        this.allFileToPrint = data;
-
+        
+        this.tempFileToPrint = data;
+        
+        
+             
+        console.log("allfiletoprint");
         console.log(data);
-
-
       },
       error => {
         console.log(error);
         alert(error);
         throw error;
+      }
+    );
 
+    //get all files to print
+    this.comService.getAllFileToPrint().subscribe(
+      (data) => {
+        
+        
+        this.allFileToPrint = data;
+        
+             
+        console.log("allfiletoprint");
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+        alert(error);
+        throw error;
       }
     );
 
@@ -263,195 +116,178 @@ export class FileToPrintSettingsComponent implements OnInit, AfterViewChecked, A
       (data) => {
         if(data!=null){
           this.allShActivities = data;
-
+             
+          console.log("allshavtivities");
           console.log(data);
         }else{
           alert("Une erreur s'est produite.Veuillez réessayer plus tard");
-        }
-    
-
-
+        } 
       },
       error => {
         console.log(error);
         alert(error);
         throw error;
-
       }
     );
+    
   }
-
-/*getStructureByActivity
-getStructureByActivity($event:any){
-  console.log($event)
-  let activity:ShActivity={
-    "descactivity":$event.tab.textLabel,
-    "idactivity":$event,
-    "statusactivity":""
-  }
-    this.comService.getStructureByActivity(activity).subscribe(
-      (data) => {
-
-      if(data!=null){
-        this.strucures= data;
-
-        console.log(data);
-      }else{
-        alert("Une erreur s'est produite.Veuillez réessayer plus tard");
-      }
   
 
-
-    },
-    error => {
-      console.log(error);
-      alert(error);
-      throw error;
-
-    }
-    );
-}*/
-  //sauvgarder les file to print
-  SelectFileToPrint() {
-
-    this.comService.saveFileToPrint(this.finalEtatSelected).subscribe(
-      data => {
-        console.log(data);
-        if (data === null) {
-          alert("Une erreur s'est produite.Veuillez réessayer plus tard");
-          setTimeout(function () {
-            window.location.reload();
-          }, 2000);
-        } else {
-          this.showAlert();
-          this.finalEtatSelected.slice(1, this.finalEtatSelected.length);
-          setTimeout(function () {
-            window.location.reload();
-          }, 2000);
-        }
-
-      },
-      error => {
-        console.log(error);
-        alert(error);
-        throw error;
-      });
-
-
-
-    this.comService.deleteFileToPrint(this.finalEtatDeselected).subscribe(
-      data => {
-        console.log(data);
-        if (data === null) {
-          alert("Une erreur s'est produite.Veuillez réessayer plus tard");
-          setTimeout(function () {
-            window.location.reload();
-          }, 2000);
-        } else {
-          this.showAlert();
-          this.finalEtatSelected.splice(1, this.finalEtatDeselected.length);
-          setTimeout(function () {
-            window.location.reload();
-          }, 2000);
-        }
-
-      },
-      error => {
-        console.log(error);
-        alert(error);
-        throw error;
-      });
-
-
-  }
-
-
-
-
-
-
-
-  updateCheckBoxes() {
-    if (this.initialisation) {
-      this.selectedCheckBoxes.splice(0, this.selectedCheckBoxes.length);
-
-
-      for (let i = 0; i < this.strucures.length; i++) {
-        for (let j = 0; j < this.toutLesEtats.length; j++) {
-          if (this.isChecked(this.strucures[i].idstructure, this.toutLesEtats[j].idfiletype)) {
-            this.selectedCheckBoxes.push(1);
-          } else {
-            this.selectedCheckBoxes.push(-1);
-          }
+  ischeked(idetat:number,idpayrollstructure:number){
+    for (let i = 0; i < this.tempFileToPrint.length; i++) {
+      if (this.tempFileToPrint[i].idStructure == idpayrollstructure) {
+        if (this.tempFileToPrint[i].idFileType==idetat) {
+          return true;
         }
       }
-
-      this.checkboxes.forEach((element, k: number) => {
-
-        if (this.selectedCheckBoxes[k] == 1) {
-          element.checked = true;
-
-        }
-
-      });
-
-      console.log(this.selectedCheckBoxes);
-      this.initialisation = false;
     }
-
-
+    return false;
   }
-  //alert pour le FileToPrint selection
-  showAlert() {
-    mobiscroll.alert({
-      title: 'Fichier à imprimer',
-      message: "Vos modification ont bien été enregistrées"
-      /* ,callback: function () {
-           mobiscroll.toast({
-               message: 'Alert closed'
-           });
-       }*/
-    });
+  
+ 
+  ischekedindex(idetat:number,idpayrollstructure:number){
+    for (let i = 0; i < this.tempFileToPrint.length; i++) {
+      if (this.tempFileToPrint[i].idStructure == idpayrollstructure) {
+        if (this.tempFileToPrint[i].idFileType==idetat) {
+          return i;
+        }
+      }
+    }
+    return -1;
   }
 
-  updateSelectedEtatForStructure(idEtat: number, idStructure: number) {
+
+  updateSelectedEtatForStructure(idetat:number,idpayrollstructure:number){
     let file: FileToPrint = {
-      "idFileType": idEtat,
-      "idStructure": idStructure,
+      "idFileType": idetat,
+      "idStructure": idpayrollstructure,
       "addedDate": new Date()
     }
-
-    this.intermediaireFileToPrint = this.allFileToPrint;
-    for (let i = 0; i < this.intermediaireFileToPrint.length; i++) {
-
-      if (this.intermediaireFileToPrint[i].idStructure == idStructure && this.intermediaireFileToPrint[i].idFileType == idEtat) {
-        this.exist = true;
-        this.index = i;
-      }
+    console.log("before");
+    console.log(this.tempFileToPrint);
+    let index=this.ischekedindex(idetat,idpayrollstructure)
+    if(index!=-1){
+      this.tempFileToPrint.splice(index,1);
+    }else{
+      this.tempFileToPrint.push(file);
+    }
+    console.log("the file");
+    console.log(file);
+    console.log("after");
+    console.log(this.tempFileToPrint);
     }
 
-
-    if (this.exist) {
-      this.finalEtatDeselected.push(file);
-      this.intermediaireFileToPrint.splice(this.index, 1);
-      this.exist = false;
-    } else {
-
-      for (let i = 0; i < this.finalEtatDeselected.length; i++) {
-        if (this.finalEtatDeselected[i].idStructure == idStructure && this.finalEtatDeselected[i].idFileType == idEtat) {
-          this.finalEtatDeselected.splice(i, 1);
-
-          this.dontPush = true;
-        }
-      }
-      if (!this.dontPush) {
-
-        this.finalEtatSelected.push(file);
-      }
-
+ 
+  //alert pour le FileToPrint selection
+  showAlert() {
+    if(this.messageAdded &&this.messageDeleted){
+      mobiscroll.alert({
+        title: 'Fichier à imprimer',
+        message: "Vos modification ont bien été enregistrées"
+        /* ,callback: function () {
+             mobiscroll.toast({
+                 message: 'Alert closed'
+             });
+         }*/
+      });
     }
-    console.log(this.finalEtatDeselected);
-    console.log(this.finalEtatSelected);
+   
   }
 
+
+  SelectFileToPrint(){
+    console.log("temp");
+    console.log(this.tempFileToPrint);
+    console.log("all");
+    console.log(this.allFileToPrint);
+    
+    let localtempFileToPrint:FileToPrint[]=this.tempFileToPrint;
+    let localallFileToPrint:FileToPrint[]=this.allFileToPrint;
+
+ 
+    // to drop 
+    const drop = this.allFileToPrint.filter(function(o1){
+      return !localtempFileToPrint.some(function(o2){    //  for diffrent we use NOT (!) befor obj2 here
+        return o1.idFileType == o2.idFileType && o1.idStructure == o2.idStructure;          // id is unnique both array object
+      });
+    });
+
+    console.log("to delete"); 
+    console.log(drop);
+
+    // Need differnt obj 
+    const add = this.tempFileToPrint.filter(function(o1){
+      return !localallFileToPrint.some(function(o2){    //  for diffrent we use NOT (!) befor obj2 here
+        return o1.idFileType == o2.idFileType && o1.idStructure == o2.idStructure;          // id is unnique both array object
+      });
+    });
+    console.log("to add"); 
+    console.log(add);
+    console.log("all and temp"); 
+    console.log(this.allFileToPrint); 
+    console.log(this.tempFileToPrint); 
+    this.SaveChangesInDB(add,drop);
+  }
+
+
+  //sauvgarder les file to print
+  SaveChangesInDB(fileToAdd:FileToPrint[],fileToDelete:FileToPrint[]) {
+
+    this.comService.saveFileToPrint(fileToAdd).subscribe(
+      data => {
+        console.log(data);
+        if (data === null) {
+          alert("Une erreur s'est produite.Veuillez réessayer plus tard");
+          setTimeout(function () {
+            window.location.reload();
+          }, 2000);
+        } else {
+          this.messageAdded=true;
+         this.showAlert();
+          fileToAdd.slice(1, fileToAdd.length);
+          
+        }
+
+      },
+      error => {
+        console.log(error);
+        alert(error);
+        throw error;
+      });
+
+
+
+    this.comService.deleteFileToPrint(fileToDelete).subscribe(
+      data => {
+        console.log(data);
+        if (data === null) {
+          alert("Une erreur s'est produite.Veuillez réessayer plus tard");
+          setTimeout(function () {
+            window.location.reload();
+          }, 2000);
+        } else {
+          this.messageDeleted=true;
+          this.showAlert();
+          fileToDelete.splice(1, fileToDelete.length);
+         
+        }
+
+      },
+      error => {
+        console.log(error);
+        alert(error);
+        throw error;
+      });
+
+     
+    
+
+  
+  }
+
+ 
+
+
+
+  
 }
