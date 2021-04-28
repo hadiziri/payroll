48b271874e98771f4c7465f53b622ddc08ccda4e
sonatrachhq <sonatrachhq@gmail.com>
2021-04-28 09:43:04 +0100

    10.1

--- backend/src/main/resources/convert.bat
+++ backend/src/main/resources/convert.bat
@@ -1,4 +1,6 @@
-C:\Users\SON8425\Desktop\Setups\LibreOfficePortable\App\libreoffice\program\scalc --invisible --convert-to dbf %1  --outdir %2
+#C:\Users\SON8425\Desktop\Setups\LibreOfficePortable\App\libreoffice\program\scalc --invisible --convert-to dbf %1  --outdir %2
+/opt/libreoffice7.1/program/soffice --calc --headless --invisible --convert-to dbf "$1"  --outdir "$2"
+
 
 
 
